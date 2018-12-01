47e790f090087e2768bb9c83efcfc12235d33644
mich101mich <mich101mich@gmail.com>
2018-12-01 09:37:03 +0100

    day 1.2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::str::FromStr;
+use std::collections::HashSet;
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("input/day_1.txt");
     let mut f = 0;
-    for n in input.lines() {
-        if n.starts_with("+") {
-            f += i32::from_str(&n[1..]).unwrap();
-        } else {
-            f -= i32::from_str(&n[1..]).unwrap();
+    let mut history = HashSet::new();
+    loop {
+        for n in input.lines() {
+            if n.starts_with("+") {
+                f += i32::from_str(&n[1..]).unwrap();
+            } else {
+                f -= i32::from_str(&n[1..]).unwrap();
+            }
+            if !history.insert(f) {
+                println!("{}", f);
+                return;
+            }
         }
     }
-    println!("{}", f);
 }
