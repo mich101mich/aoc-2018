3490b9ef476a0036307592a2c70d13e87f43a2ad
mich101mich <mich101mich@gmail.com>
2018-12-02 10:24:51 +0100

    day 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
+#![allow(unused_imports)]
+
 use std::str::FromStr;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
-    let input = include_str!("input/day_1.txt");
-    let mut f = 0;
-    let mut history = HashSet::new();
-    loop {
-        for n in input.lines() {
-            if n.starts_with("+") {
-                f += i32::from_str(&n[1..]).unwrap();
+    let input = include_str!("input/day_2.txt");
+    
+    let mut twos = 0;
+    let mut threes = 0;
+
+    for line in input.lines() {
+        let mut count = HashMap::<char, usize>::new();
+        for c in line.chars() {
+            if count.contains_key(&c) {
+                *count.get_mut(&c).unwrap() += 1;
             } else {
-                f -= i32::from_str(&n[1..]).unwrap();
+                count.insert(c, 1);
             }
-            if !history.insert(f) {
-                println!("{}", f);
-                return;
+        }
+        let mut found_two = false;
+        let mut found_three = false;
+        for i in count.values() {
+            if *i == 2 {
+                if !found_two {
+                    twos += 1;
+                    found_two = true;
+                }
+            } else if *i == 3 {
+                if !found_three {
+                    threes += 1;
+                    found_three = true;
+                }
             }
         }
     }
+    println!("{}", twos);
+    println!("{}", threes);
+    println!("{}", twos * threes);
 }
