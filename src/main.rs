76c846f1a68ceb9aa57ccb45da5198afd52c98dd
mich101mich <mich101mich@gmail.com>
2018-12-03 08:41:42 +0100

    day 3.1

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,36 @@ use std::collections::HashSet;
 use std::str::FromStr;
 
 fn main() {
-    let input = include_str!("input/day_2.txt");
+    let input = include_str!("input/day_3.txt");
 
-    for line1 in input.lines() {
-        for line2 in input.lines() {
-            let mut diff = 0;
-            for (c1, c2) in line1.chars().zip(line2.chars()) {
-                if c1 != c2 {
-                    diff += 1;
+    let mut grid: Vec<Vec<usize>> = std::iter::repeat(
+        std::iter::repeat(0).take(1000).collect::<Vec<usize>>(),
+    ).take(1000)
+        .collect();
+    let mut count = 0;
+
+    for line in input.lines() {
+        let line = line.split(" @ ").skip(1).next().unwrap(); // 170,644: 29x14
+        let x = usize::from_str(line.split(",").next().unwrap()).unwrap();
+        let line = line.split(",").skip(1).next().unwrap();
+        let y = usize::from_str(line.split(": ").next().unwrap()).unwrap();
+        let size = line
+            .split(": ")
+            .skip(1)
+            .next()
+            .unwrap()
+            .split("x")
+            .collect::<Vec<&str>>();
+        let width = usize::from_str(size[0]).unwrap();
+        let height = usize::from_str(size[1]).unwrap();
+        for dx in x..(x + width) {
+            for dy in y..(y + height) {
+                grid[dx][dy] += 1;
+                if grid[dx][dy] == 2 {
+                    count += 1;
                 }
             }
-            if diff != 1 {
-                continue;
-            }
-            println!("{}", line1);
-            println!("{}", line2);
-            return;
         }
     }
+    println!("{}", count);
 }
