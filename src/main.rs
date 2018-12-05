cf099be7e4803f8704a91c8c15518fb1acbd08c5
mich101mich <mich101mich@gmail.com>
2018-12-05 19:17:38 +0100

    day 5.2

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,48 @@ use std::str::FromStr;
 
 fn main() {
     let input = include_str!("input/day_5.txt");
-    let mut input = input.chars().collect::<Vec<_>>();
 
-    let mut change = true;
-    while change {
-        change = false;
-        for i in 0..(input.len() - 1) {
-            if input[i].is_lowercase() {
-                if input[i].to_uppercase().next().unwrap() == input[i + 1] {
-                    input.remove(i);
-                    input.remove(i);
-                    change = true;
-                    break;
+    let mut min_c = 'a';
+    let mut min = input.len();
+
+    for c in 0..26 {
+        let c = (c + 97u8) as char;
+        let upper = c.to_uppercase().next().unwrap();
+        let mut input = input
+            .chars()
+            .filter(|i| *i != c && *i != upper)
+            .collect::<Vec<_>>();
+
+        let mut change = true;
+        while change {
+            change = false;
+            let mut i = 0;
+            while i < input.len() - 1 {
+                if input[i].is_lowercase() {
+                    if input[i].to_uppercase().next().unwrap() == input[i + 1] {
+                        input.remove(i);
+                        input.remove(i);
+                        change = true;
+                        i -= 2;
+                    }
+                } else {
+                    if input[i].to_lowercase().next().unwrap() == input[i + 1] {
+                        input.remove(i);
+                        input.remove(i);
+                        change = true;
+                        i -= 2;
+                    }
                 }
-            } else {
-                if input[i].to_lowercase().next().unwrap() == input[i + 1] {
-                    input.remove(i);
-                    input.remove(i);
-                    change = true;
-                    break;
-                }
+                i += 1;
             }
         }
+        println!("{}: {}", c, input.len());
+        if input.len() < min {
+            min = input.len();
+            min_c = c;
+        }
     }
-    println!("{}", input.len());
+    println!("min: {}: {}", min_c, min);
 }
 
 #[allow(unused)]
