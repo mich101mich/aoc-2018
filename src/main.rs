a840f2eb3588d4bc8a8863ca103f9d4395325193
mich101mich <mich101mich@gmail.com>
2018-12-12 08:49:58 +0100

    day 12.1

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,56 @@ use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
-    let input: i32 = include!("input/day_11.txt");
-    //let input = 42_i32;
+    let input = include_str!("input/day_12.txt");
+    let lines = input.lines().collect::<Vec<_>>();
+    let initial = &lines[0][15..];
+    let lines = &lines[2..];
 
-    let mut grid = get_grid(0_i32, 300, 300);
+    let mut patterns = HashMap::new();
+    for line in lines {
+        patterns.insert(&line[0..5], line.chars().nth(9).unwrap());
+    }
 
-    for y in 1..=300 {
-        let mut row_sum = 0;
-        for x in 1..=300 {
-            let rack_id = x + 10;
-            let mut pow = rack_id * y + input;
-            pow *= rack_id;
-            pow = (pow / 100) % 10 - 5;
-            let (x, y) = (x as usize - 1, y as usize - 1);
-            row_sum += pow;
-            grid[x][y] = row_sum + if y > 0 { grid[x][y - 1] } else { 0 };
+    let mut pots = HashMap::new();
+
+    for (i, c) in initial.chars().enumerate() {
+        if c == '#' {
+            pots.insert(i as i32, c);
         }
     }
-    let mut max = 0;
-    let mut max_xy = (0, 0, 0);
-    for size in 2..=300 {
-        println!("{}", size);
-        for y in size..300 {
-            for x in size..300 {
-                let sum =
-                    grid[x][y] - grid[x - size][y] - grid[x][y - size] + grid[x - size][y - size];
-                if sum > max {
-                    max = sum;
-                    max_xy = (x - size + 2, y - size + 2, size);
+
+    let mut first: i32 = *pots.keys().min().unwrap();
+    let mut last: i32 = *pots.keys().max().unwrap();
+
+    for gen in 0..20 {
+        let old = pots.clone();
+        for p in (first - 4)..(last + 4) {
+            let pat: &str = &format!(
+                "{}{}{}{}{}",
+                old.get(&(p - 2)).unwrap_or(&'.'),
+                old.get(&(p - 1)).unwrap_or(&'.'),
+                old.get(&p).unwrap_or(&'.'),
+                old.get(&(p + 1)).unwrap_or(&'.'),
+                old.get(&(p + 2)).unwrap_or(&'.')
+            );
+            if patterns[pat] == '#' {
+                pots.insert(p, '#');
+                if p < first {
+                    first = p;
                 }
+                if p > last {
+                    last = p;
+                }
+            } else {
+                pots.remove(&p);
             }
         }
+        if gen % 1_000_000 == 0 {
+            println!("{}", gen);
+        }
     }
-    println!("{}", max);
-    println!("{},{},{}", max_xy.0, max_xy.1, max_xy.2);
+    let cnt: i32 = pots.keys().sum();
+    println!("{}", cnt);
 }
 
 #[allow(unused)]
