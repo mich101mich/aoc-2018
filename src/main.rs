bc82d5d962d257a1234dc55cf5151fb530a75532
mich101mich <mich101mich@gmail.com>
2018-12-13 08:13:41 +0100

    day 13.1

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,155 @@ use std::collections::HashSet;
 use std::io::Write;
 use std::str::FromStr;
 
+enum Dir {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+use crate::Dir::*;
+
 fn main() {
-    let input = include_str!("input/day_12.txt");
-    let lines = input.lines().collect::<Vec<_>>();
-    let initial = &lines[0][15..];
-    let lines = &lines[2..];
+    let input = include_str!("input/day_13.txt");
 
-    let mut patterns = HashMap::new();
-    for line in lines {
-        patterns.insert(&line[0..5], line.chars().nth(9).unwrap());
-    }
+    let mut grid = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let mut carts = vec![];
 
-    let mut pots = HashMap::new();
+    let h = grid.len();
+    let w = grid[0].len();
 
-    for (i, c) in initial.chars().enumerate() {
-        if c == '#' {
-            pots.insert(i as i32, c);
-        }
-    }
-
-    let mut first: i32 = *pots.keys().min().unwrap();
-    let mut last: i32 = *pots.keys().max().unwrap();
-
-    for gen in 0..20 {
-        let old = pots.clone();
-        for p in (first - 4)..(last + 4) {
-            let pat: &str = &format!(
-                "{}{}{}{}{}",
-                old.get(&(p - 2)).unwrap_or(&'.'),
-                old.get(&(p - 1)).unwrap_or(&'.'),
-                old.get(&p).unwrap_or(&'.'),
-                old.get(&(p + 1)).unwrap_or(&'.'),
-                old.get(&(p + 2)).unwrap_or(&'.')
-            );
-            if patterns[pat] == '#' {
-                pots.insert(p, '#');
-                if p < first {
-                    first = p;
+    for y in 0..h {
+        for x in 0..w {
+            match grid[y][x] {
+                '>' => {
+                    carts.push((x, y, Right, 0));
+                    grid[y][x] = '-';
                 }
-                if p > last {
-                    last = p;
+                '<' => {
+                    carts.push((x, y, Left, 0));
+                    grid[y][x] = '-';
                 }
-            } else {
-                pots.remove(&p);
+                '^' => {
+                    carts.push((x, y, Up, 0));
+                    grid[y][x] = '|';
+                }
+                'v' => {
+                    carts.push((x, y, Down, 0));
+                    grid[y][x] = '|';
+                }
+                _ => {}
             }
         }
-        if gen % 1_000_000 == 0 {
-            println!("{}", gen);
+    }
+
+    let mut count = 0;
+    loop {
+        count += 1;
+
+        carts.sort_by_key(|cart| cart.0 + cart.1 * h);
+
+        for i in 0..carts.len() {
+            {
+                let cart = &mut carts[i];
+                match cart.2 {
+                    Right => {
+                        cart.0 += 1;
+                        cart.2 = next_dir(grid[cart.1][cart.0], &Right, &mut cart.3);
+                    }
+                    Left => {
+                        cart.0 -= 1;
+                        cart.2 = next_dir(grid[cart.1][cart.0], &Left, &mut cart.3);
+                    }
+                    Up => {
+                        cart.1 -= 1;
+                        cart.2 = next_dir(grid[cart.1][cart.0], &Up, &mut cart.3);
+                    }
+                    Down => {
+                        cart.1 += 1;
+                        cart.2 = next_dir(grid[cart.1][cart.0], &Down, &mut cart.3);
+                    }
+                }
+            }
+            for j in 0..carts.len() {
+                if i != j && carts[i].0 == carts[j].0 && carts[i].1 == carts[j].1 {
+                    println!("{},{}", carts[i].0, carts[i].1);
+                    return;
+                }
+            }
         }
     }
-    let cnt: i32 = pots.keys().sum();
-    println!("{}", cnt);
+}
+
+fn next_dir(c: char, current: &Dir, intersect: &mut usize) -> Dir {
+    match current {
+        Right => match c {
+            '/' => Up,
+            '\\' => Down,
+            '+' => {
+                let d;
+                match intersect {
+                    0 => d = Up,
+                    1 => d = Right,
+                    _ => d = Down,
+                }
+                *intersect = (*intersect + 1) % 3;
+                d
+            }
+            '-' => Right,
+            _ => panic!("invalid char on track"),
+        },
+        Left => match c {
+            '/' => Down,
+            '\\' => Up,
+            '+' => {
+                let d;
+                match intersect {
+                    0 => d = Down,
+                    1 => d = Left,
+                    _ => d = Up,
+                }
+                *intersect = (*intersect + 1) % 3;
+                d
+            }
+            '-' => Left,
+            _ => panic!("invalid char on track"),
+        },
+        Up => match c {
+            '/' => Right,
+            '\\' => Left,
+            '+' => {
+                let d;
+                match intersect {
+                    0 => d = Left,
+                    1 => d = Up,
+                    _ => d = Right,
+                }
+                *intersect = (*intersect + 1) % 3;
+                d
+            }
+            '|' => Up,
+            _ => panic!("invalid char on track"),
+        },
+        Down => match c {
+            '/' => Left,
+            '\\' => Right,
+            '+' => {
+                let d;
+                match intersect {
+                    0 => d = Right,
+                    1 => d = Down,
+                    _ => d = Left,
+                }
+                *intersect = (*intersect + 1) % 3;
+                d
+            }
+            '|' => Down,
+            _ => panic!("invalid char on track"),
+        },
+    }
 }
 
 #[allow(unused)]
